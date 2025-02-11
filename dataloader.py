93d60cb7fbf6b15339faad1682cbab7a2baaf34d
Runlong (Mike) He <1291461377@qq.com>
2025-02-11 18:58:33 +0000

    Add code for PitVQA++

---
+++ dataloader.py
@@ -0,0 +1,49 @@
+import os
+import glob
+
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from pathlib import Path
+
+
+class PitVQASentence(Dataset):
+    def __init__(self, seq, folder_head, folder_tail):
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+	        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        # files, question and answers
+        filenames = []
+        for curr_seq in seq:
+            filenames = filenames + glob.glob(folder_head + str(curr_seq) + folder_tail)
+        self.vqas = []
+        for file in filenames:
+            file_data = open(file, "r")
+            lines = [line.strip("\n") for line in file_data if line != "\n"]
+            file_data.close()
+            for line in lines:
+                self.vqas.append([file, line])
+        print('Total files: %d | Total question: %.d' % (len(filenames), len(self.vqas)))
+
+    def __len__(self):
+        return len(self.vqas)
+
+    def __getitem__(self, idx):
+        qa_full_path = Path(self.vqas[idx][0])
+        seq_path = qa_full_path.parents[3]
+        video_folder = qa_full_path.parts[-2]
+
+        img_path = seq_path / 'images' / video_folder
+        file_name = self.vqas[idx][0].split('/')[-1] 
+
+        # img
+        img_loc = os.path.join(img_path, file_name.split('.')[0] + '.png')
+        raw_image = Image.open(img_loc).convert('RGB')
+        img = self.transform(raw_image)
+
+        # question and answer
+        question, answer = self.vqas[idx][1].split('|')
+        return img, question, answer
---
+++ utils.py
@@ -0,0 +1,48 @@
+import torch
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_recall_fscore_support
+
+
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def save_clf_checkpoint(checkpoint_dir, model):
+    """
+    Save model state dict
+    """
+    filename = checkpoint_dir + 'Best.pth.tar'
+    torch.save(model.state_dict(), filename)
+
+
+def adjust_learning_rate(optimizer, shrink_factor):
+    """
+    Shrinks learning rate by a specified factor.
+
+    :param optimizer: optimizer whose learning rate must be shrunk.
+    :param shrink_factor: factor in interval (0, 1) to multiply learning rate with.
+    """
+    print("\nDECAYING learning rate.")
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = param_group['lr'] * shrink_factor
+    print("The new learning rate is %f\n" % (optimizer.param_groups[0]['lr'],))
